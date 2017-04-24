6ba529a8a8eb2a644a7dca9d0da51b38b58bd186
Victor Gamez <victorgamez@MacBook-Pro-de-Victor.local>
2017-04-24 16:05:43 +0200

    jsdocsstroy

--- client/views/story/story.js
+++ client/views/story/story.js
@@ -1,6 +1,14 @@
 
 
 Template.story.events({
+
+  /**
+     * @summary Event on click. Update collection Storis with 'like' in one story
+     * @isMethod true
+     * @locus Template.story.events
+     * @param  {Object} event need event for update
+     */
+
     'click .like':function(e) {
         e.preventDefault();
         var story = Blaze.getData(e.currentTarget);
@@ -13,12 +21,26 @@ Template.story.events({
             Stories.update({_id: story._id}, {$pull:{likes:likeData}});
         }
     },
+
+    /**
+       * @summary Event on click. Delete one story
+       * @isMethod true
+       * @locus Template.story.events
+       * @param  {Object} event need event for remove
+       */
     'click .delete-story':function(e){
       e.preventDefault();
       var story = Blaze.getData(e.currentTarget);
       //console.log(story);
       Stories.remove(story._id);
     },
+
+    /**
+       * @summary Event submit. Inset new comment in one story
+       * @isMethod true
+       * @locus Template.story.events
+       * @param  {Object} event need event for insert
+       */
     'submit .comment-story':function(event){
       event.preventDefault();
 
@@ -54,14 +76,32 @@ Template.story.events({
 
 })
 
+
+
 Template.story.helpers({
+  /**
+    * @summary anithing
+    * @isMethod true
+    * @locus Template.story.helpers
+    */
     status:function(){
         return this.createdFor === this.createdBy;
     },
+
+    /**
+       * @summary Compare owner with this user conectet
+       * @isMethod true
+       * @locus Template.story.helpers
+       */
     ownerStory:function(){
         return this.createdBy === Meteor.userId();
     },
 
+    /**
+       * @summary Return True or False if this user put one 'like' or not
+       * @isMethod true
+       * @locus Template.story.helpers
+       */
     styleLike:function(){
 
       var liker = Meteor.user();
@@ -74,10 +114,22 @@ Template.story.helpers({
       }
     },
 
+    /**
+       * @summary Function to refactor the date of the story
+       * @isMethod true
+       * @locus Template.story.helpers
+       * @param  {Date} date Need date story for refactor it and show it
+       */
     datestory:function(date) {
       return moment(date).format('MM-DD-YYYY HH:mm');
     },
 
+    /**
+       * @summary Function with 'case' that return how many 'likes' has this story
+       * @isMethod true
+       * @locus Template.story.helpers
+       * @param  {Integer} storyId Id story
+       */
     likeCount:function(storyId){
         var story = Stories.findOne({_id: storyId});
         var likes = story.likes;
@@ -112,7 +164,15 @@ Template.story.helpers({
 
 })
 
+
+
 Template.commentTemaplte.events({
+  /**
+     * @summary Event on click. Delete one comment from stroy
+     * @isMethod true
+     * @locus Template.commentTemaplte.events
+     * @param  {Object} e need event for delete
+     */
   'click .delete-comment':function(e){
     e.preventDefault();
     var comment = Blaze.getData(e.currentTarget);
@@ -131,12 +191,25 @@ Template.commentTemaplte.events({
   }
 })
 
+
+
 //Need this package meteor add momentjs:moment
 Template.commentTemaplte.helpers({
+  /**
+     * @summary Function to refactor the date of the comment
+     * @isMethod true
+     * @locus Template.commentTemaplte.helpers
+     * @param  {Date} date Need date comment for refactor it and show it
+     */
   dateRefactor:function(date){
 
     return moment(date).format('MM-DD-YYYY HH:mm');
   },
+  /**
+     * @summary Return true if this comment are from the user conectecd
+     * @isMethod true
+     * @locus Template.commentTemaplte.helpers
+     */
   ownerComment:function(){
       return this.owner === Meteor.userId();
   }
